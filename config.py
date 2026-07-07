6b36b4a2ce414c5c90f26d97e489927ad6b0425c
MIRIYALA  INDU  VARDHAN <66687574+indu134@users.noreply.github.com>
2026-07-07 12:59:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ dQIDAQAB
 # 4. Q3: 12-Factor Config (Manually merge the variables)
 Q3_PORT = 8102
 Q3_WORKERS = 1
-Q3_DEBUG = "true"
+Q3_DEBUG = True
 Q3_LOG_LEVEL = "info"
 
 # 5. Q5: Analytics (Find the API key in the Q5 instruction tab)
